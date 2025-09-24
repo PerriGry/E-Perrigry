e4c3d21d5900c8892be4fb00de1de74efc76859c
Cardeniu <juandavidcarderubio502@gmail.com>
2025-09-24 10:25:48 -0500

    register_not_finished

---
+++ src/components/common/inputUser.jsx
@@ -0,0 +1,9 @@
+
+export default function inputUser() {
+    return (
+        <div>
+            <p className="text-2xl font-bold">Usuario</p>
+            <input type="text" placeholder="Ingresa tu usuario" className="border border-gray-300 rounded-md p-2 w-70 mb-4" />
+        </div>
+    )
+}
---
+++ src/components/index.jsx
@@ -0,0 +1,11 @@
+import inputUser from "./common/inputUser";
+import inputPassword from "./common/inputPassword";
+import buttonSubmit from "./common/buttonSubmit";
+import inputEmail from "./common/inputEmail";
+
+export { 
+    inputUser,
+    inputPassword,
+    buttonSubmit,
+    inputEmail
+};
