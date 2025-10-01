9aea0838c2bce92356a0d5f2a3b96935d88cd6e9
Cardeniu <juandavidcarderubio502@gmail.com>
2025-10-01 08:01:18 -0500

    fix

--- src/components/common/buttonSubmit.jsx
+++ src/components/common/buttonSubmit.jsx
@@ -1,9 +1,7 @@
-
-
-    export default function buttonSubmit() {
+export default function buttonSubmit() {
     return (
         <button type="submit" className="bg-blue-400 text-white rounded-md p-2 w-50 hover:bg-blue-600 transition-colors duration-150 ease-in-out cursor-pointer">
             Registrarse
         </button>
     );
-    }
+}
--- src/components/common/inputPassword.jsx
+++ src/components/common/inputPassword.jsx
@@ -1,10 +1,10 @@
 
 
-export default function inputPassword() {
-    return (
-        <div>
-            <p className="text-2xl font-bold">Contraseña</p>
-            <input type="password" placeholder="Ingresa tu contraseña" className="border border-gray-300 rounded-md p-2 w-70 mb-4" />
-        </div>
-    );
-}
+    export default function inputPassword() {
+        return (
+            <div>
+                <p className="text-2xl font-bold">Contraseña</p>
+                <input type="password" placeholder="Ingresa tu contraseña" className="border border-gray-300 rounded-md p-2 w-70 mb-4" />
+            </div>
+        );
+    }
